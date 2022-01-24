16efd2ea588def99be5fca175b8403080ac74478
mariaacanfora <ma.maria.acanfora@gmail.com>
2022-01-24 17:31:06 +0100

    Created models and migrations of movies and categories

---
+++ app/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = ['title', 'description', 'coverImg', 'date', 'duration', 'rating'];
+
+    public function categories(){
+        return $this->belongsToMany('App\Category');
+    }
+}
