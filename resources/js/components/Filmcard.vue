d4bc625c5be43ff3b6f4a331355f4046fa94cfd1
FrancescoAndreotti <francescoandreotti00@gmail.com>
2022-01-27 18:04:15 +0100

    Component FIlmCard

---
+++ resources/js/components/Filmcard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="card">
+      <img :src="{ filmImg }" alt="img" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">{{ filmTitle }}</h5>
+        <button class="btn">+ Watchlist</button>
+        <a :href="{ filmTrailer }">Trailer</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
